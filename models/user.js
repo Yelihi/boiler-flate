8f097a179884ec14dd8607d6d02fba0bd66781d4
Yelihi <Yelihi19@gmail.com>
2022-07-11 00:09:31 +0900

    Add: 비밀번호 난수화

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // 10자리인 salt 를 만들어서 비밀번호 암호화 함.
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,6 +33,25 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+        // Store hash in your password DB.
+      });
+    });
+  } else {
+    next();
+  }
+  // 비밀번호를 암호화 시킨다.
+}); // 몽구스에서 가져온 메서드. 유저 정보를 저장하기 전에 무엇을 한다.
 
 const User = mongoose.model("User", userSchema);
 
