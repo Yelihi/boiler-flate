59cb8965f8f4d935762da0711dcc927184e58402
Yelihi <Yelihi19@gmail.com>
2022-07-14 21:00:18 +0900

    Add: logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,25 @@
 import React, { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/users/hello").then((response) => {
       console.log(response.data);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((res) => {
+      if (res.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 하는데 실패했습니다.");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +32,8 @@ export default function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onEmailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onNameHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onConfirmPasswordHandler = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호가 일치하지 않습니다.");
+    }
+
+    let body = {
+      email: Email,
+      password: Password,
+      name: Name,
+    };
+
+    dispatch(registerUser(body)).then((res) => {
+      if (res.payload.success) {
+        navigate("/login"); // 페이지 이동 시킬때
+      } else {
+        alert("error");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+        <button>회원가입</button>
+      </form>
+    </div>
+  );
 }
